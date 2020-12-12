595af4f7586e4a546a5cea1a643471150a866acf
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2020-12-12 10:33:11 -0700

    update to palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -10,6 +10,16 @@ function palindrome(string) {
 //Defines a Phrase object.
 function Phrase(content) {
     this.content = content;
+    //Defines a TranslatedPhrase object.
+    function TranslatedPhrase(content, translation) {
+        this.content=content;
+        this.translation = translation;
+    }
+    TranslatedPhrase.prototype = new Phrase();
+    //Returns content processed for palindrome testing.
+    this.processecContent = function processedContent() {
+        return this.content.toLowerCase();
+    }
 
 //Returns true if the phrase is a palindrome, false otherwise.
 this.palindrome = function palindrome() {
