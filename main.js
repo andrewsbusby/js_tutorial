521d14d14a419070d847c2ea8d12b65e80946ba9
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-01-08 01:35:47 -0700

    testing

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let Phrase = require("abusby-palindrome");
+import Phrase, { palindrome } from "abusby-palindrome";
 
 let string = prompt("Please enter a string for palindrome testing:");
 let Phrase = new Phrase(string);
 
-if (Phrase.palindrome()) {
+if (palindrome()) {
     alert(`"${phrase.content}" is a palindrome!`);
 } else {
     alert(`"${phrase.content}" is not a palindrome.`)
