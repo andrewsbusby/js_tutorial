de1031421b1929da78249336b78b6597acac0553
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-01-08 00:51:02 -0700

    update to main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let Phrase = require("abusby-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Please enter a string for palindrome testing:");
+let Phrase = new Phrase(string);
+
+if (Phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+} else {
+    alert(`"${phrase.content}" is not a palindrome.`)
+}
