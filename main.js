f0b6a5ab1d4184803483fa520b922ecf68bfee50
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-01-15 00:19:16 -0700

    added bundle.js and fixed issue in main.js that prevented bundle

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import Phrase, { palindrome } from "abusby-palindrome";
+let Phrase = require("abusby-palindrome");
 
 let string = prompt("Please enter a string for palindrome testing:");
-let Phrase = new Phrase(string);
+let phrase = new Phrase(string);
 
-if (palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`);
 } else {
-    alert(`"${phrase.content}" is not a palindrome.`)
+  alert(`"${phrase.content}" is not a palindrome.`)
 }
