d37805c8363b6e0df54259afe9537a240c7827c2
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-02-07 01:49:47 -0700

    fixing main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let Phrase = require("<username>-palindrome");
+let Phrase = require("abusby-palindrome");
 
 function palindromeTester() {
   let string = prompt("Please enter a string for palindrome testing:");
